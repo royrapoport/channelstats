d4fffcbec003c03eedf59ff0df8cc6b522d18b6f
Roy Rapoport <rrapoport@netflix.com>
2019-08-10 08:45:50 -0700

    enable generation of report on a per-userid basis, for debugging and amusement

--- report_generator.py
+++ report_generator.py
@@ -2,11 +2,11 @@
 
 import datetime
 
-import ddb
-import messagetablefactory
-import user
 import channel
+import messagetablefactory
 import report
+import user
+import utils
 
 class ReportGenerator(object):
 
@@ -14,29 +14,40 @@ class ReportGenerator(object):
         self.mtf = messagetablefactory.MessageTableFactory()
         self.report = report.Report()
 
-    def generate_report(self, start_day, days):
+    def generate_report(self, start_day, days, user=None):
         """
         Generate a channel stats report starting on start_day, which is
         formatted as yyyy-mm-dd, and for the period of DAYS duration
         """
         dates = self.generate_dates(start_day, days)
-        print("For the report of {} days starting {} we have dates {}".format(days, start_day, dates))
+        # print("For the report of {} days starting {} we have dates {}".format(days, start_day, dates))
         tables = [self.mtf.get_message_table(date) for date in dates]
-        print("Message table names: {}".format(tables))
+        # print("Message table names: {}".format(tables))
         for table in tables:
             messages = table.scan()["Items"]
             for message in messages:
-                if message['user_id'] == "U06NSQT34":
+                if user is None:
+                    self.report.message(message)
+                elif message['user_id'] == user:
                     self.report.message(message)
         # self.report.dump()
+        self.reaction_summary(40)
+        return
         print("gm activity by hour:")
         for x in range(0,24):
             print("{}: {}".format(x, self.report._data['hour'].get(x)))
-
         print("user activity by hour:")
         for x in range(0,24):
             print("{}: {}".format(x, self.report._data['user_hour'].get(x)))
 
+    def reaction_summary(self, top=10):
+        rdict = self.report._data['reaction']
+        reactions = list(rdict.keys())
+        reactions.sort(key = lambda x: rdict[x])
+        reactions.reverse()
+        reactions = reactions[0:top]
+        for reaction in reactions:
+            print(":{}: `:{}:` :{}".format(reaction, reaction, rdict[reaction]))
 
     def generate_dates(self, start_day, days):
 
@@ -50,6 +61,11 @@ class ReportGenerator(object):
         return dates
 
 if __name__ == "__main__":
+    noemi = "UHWD9BHPD"
+    roy = "U06NSQT34"
+    jenna =  "U8MEPG4Q7"
     rg = ReportGenerator()
-    rg.generate_report("2019-07-21", 7)
-    rg.generate_report("2019-07-28", 7)
+    # rg.generate_report("2019-07-21", 7)
+    # rg.generate_report("2019-07-28", 7, roy)
+    rg.generate_report("2019-07-28", 7, jenna)
+    # rg.generate_report("2019-07-28", 7, noemi)
