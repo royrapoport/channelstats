63dd8cdd8fa095dcd518d5bf5d22edb175a49399
Roy Rapoport <rrapoport@netflix.com>
2019-08-10 05:30:54 -0700

    save user's timezone offset in seconds; provide a get() method

--- user.py
+++ user.py
@@ -7,6 +7,15 @@ class User(object):
     def __init__(self):
         self.ddb = ddb.DDB(self.table_name, [('slack_uid', 'S')], (10,10))
         self.table = self.ddb.get_table()
+        self.users = {}
+
+    def get(self, uid):
+        if uid in self.users:
+            return self.users[uid]
+        response = self.table.get_item(Key={'slack_uid':uid})
+        item = response.get("Item")
+        self.users[uid] = item
+        return item
 
     def batch_upload(self, users):
         with self.table.batch_writer() as batch:
@@ -16,7 +25,8 @@ class User(object):
                     'real_name': user.get("real_name"),
                     'name': user.get("name"),
                     'display_name': user.get('profile', {}).get('display_name'),
-                    'tz': user.get("tz")
+                    'tz': user.get("tz"),
+                    'tz_offset': user.get("tz_offset")
                 }
                 Row = self.prune_empty(Row)
                 batch.put_item(Row)
