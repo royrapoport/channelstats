6905a377d41226ca24071937b0690da5bb1753f0
Roy Rapoport <rsr@slack-corp.com>
2019-08-23 13:34:02 -0700

    allow longer backlog

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ slack_name = "rands-leadership"
 # Use DDB in local mode?
 local = True
 # How far back are we willing to go for old messages
-max_age = 20
+max_age = 60
 # What prefix, should we give DDB table names? This makes it easy to
 # have multiple channelstats instances using the same DDB environment, each
 # with its own namespace
 prefix = "channelstats"
 # By default, refetch this many days
-refetch_interval = 7
+refetch = 60
