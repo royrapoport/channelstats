b2c8ffdd5c1cce1e0ca685ac6af289fd3bb92213
Roy Rapoport <rrapoport@netflix.com>
2019-08-25 12:28:42 -0700

    support batch fetch; also store number of membrers and creation timestamp

--- channel.py
+++ channel.py
@@ -9,18 +9,25 @@ class Channel(object):
         self.ddb = ddb.DDB(self.table_name, [('key', 'S')])
         self.table = self.ddb.get_table()
 
+    def batch_get_channel(self, cids):
+        return self.ddb.batch_hash_get(cids)
+
     def batch_upload(self, channels):
         with self.table.batch_writer() as batch:
             for channel in channels:
                 cid = channel['id']
                 cname = channel.get("name")
                 created = channel['created']
-                Row = {'key': cid, 'name': cname, 'created': created}
-                Row = utils.prune_empty(Row)
-                batch.put_item(Row)
-                Row = {'key': cname, 'name': cid, 'created': created}
-                Row = utils.prune_empty(Row)
-                batch.put_item(Row)
+                members = channel['num_members']
+                values = {'created': created, 'members': members}
+                for k, v in [[cid, cname], [cname, cid]]:
+                    Row = {}
+                    Row['key'] = k
+                    Row['name'] = v
+                    for i in values:
+                        Row[i] = values[i]
+                    Row = utils.prune_empty(Row)
+                    batch.put_item(Row)
 
     def get(self, key):
         response = self.table.get_item(Key={'key': key})
