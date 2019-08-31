29b364d6ece4ccda1a7e977dfdab2fae2df2082a
Roy Rapoport <github@ols.inorganic.org>
2019-08-31 12:57:46 -0400

    minor enhancements; also try 50 times to get a unique name, rather than 10.

--- random_name.py
+++ random_name.py
@@ -1,10 +1,13 @@
+#! /usr/bin/env python
 
 import random
+import sys
 import time
 
 fname = "random_names"
 
 class RandomName(object):
+    attempts = 50
 
     def __init__(self):
         firstd = {}
@@ -23,14 +26,24 @@ class RandomName(object):
         self.failures = 0
 
     def name(self):
-        attempts = 10
-        while attempts:
+        attempt = 1
+        while attempt  < self.attempts:
             name = "{} {}".format(random.choice(self.first), random.choice(self.last))
             if name not in self.used:
                 self.used[name] = 1
                 return name
-            attempts -= 1
+            attempt += 1
             # print("Found collision on attempt {}".format(10 - attempts))
             self.collisions += 1
         self.failures += 1
         return name  # Oh well.  Giving up is better than failing to give a name
+
+if __name__ == "__main__":
+    counter = 2000000
+    if len(sys.argv) > 1:
+        counter = int(sys.argv[1])
+    rn = RandomName()
+    for i in range(counter):
+        rn.name()
+    print("{} collisions ({:.1f}%)".format(rn.collisions, rn.collisions * 100.0 / counter))
+    print("{} failures ({:.1f}%)".format(rn.failures , rn.failures * 100.0 / counter))
