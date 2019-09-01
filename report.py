c6ea49ce250747c09d0eda3c342806e3919fc566
Roy Rapoport <github@ols.inorganic.org>
2019-08-31 22:33:36 -0400

    checkpoint

--- report.py
+++ report.py
@@ -320,6 +320,22 @@ class Report(object):
             self.create_key(["reaction", reaction_name], 0)
             self._data['reaction'][reaction_name] += count
 
+    def _dfinalize_user_stats(self):
+        """
+        make sure all user_stats structures have all the fields we expect
+        """
+        # What fields do we expect?
+        expect = {}
+        for uid in self._data['user_stats']:
+            for i in self._data['user_stats'][uid].keys():
+                expect[i] = 1
+        expected_keys = list(expect.keys())
+        for uid in self._data['user_stats']:
+            struct = self._data['user_stats'][uid]
+            for expected_key in expected_keys:
+                if expected_key not in struct:
+                    struct[expected_key] = 0
+
     def accum_reaction_count(self, message):
         """
         keep track of most reacji'ed messages, keep count of
--- report_generator.py
+++ report_generator.py
@@ -21,7 +21,6 @@ class ReportGenerator(object):
     def __init__(self, fake=False):
         self.fake = fake
         self.mtf = messagetablefactory.MessageTableFactory()
-        self.report_creator = report.Report()
         self.report_store = report_store.ReportStore()
 
     def make_report_id(self, start_day, days, user):
@@ -45,7 +44,29 @@ class ReportGenerator(object):
         rid = self.make_report_id(start_day, days, user)
         self.report_store.set(rid, json.dumps(report))
 
+    def previous_report(self, start_day, days, users=None, force_generate=False):
+        """
+        Given a start day and days, get the PREVIOUS period's report with the same
+        parameters as report() takes.
+        In other words, if start_day is 2019-08-10 and days is 3, then
+        this will get the report for 2019-08-07 for 3 days
+        """
+        y, m, d = [int(x) for x in start_day.split('-')]
+        dt = datetime.date(y, m, d)
+        delta = datetime.timedelta(days=days)
+        dt -= delta
+        new_start_day = dt.strftime("%Y-%m-%d")
+        return self.get_report(new_start_day, days, users, force_generate)
+
     def report(self, start_day, days, users=None, force_generate=False):
+        """
+        Returns (current_report, previous_report)
+        """
+        current_report = self.get_report(start_day, days, users, force_generate)
+        previous_report = self.previous_report(start_day, days, users, force_generate)
+        return current_report
+
+    def get_report(self, start_day, days, users=None, force_generate=False):
         """
         Generate a channel stats report starting on start_day, which is
         formatted as yyyy-mm-dd, and for the period of DAYS duration
@@ -57,23 +78,25 @@ class ReportGenerator(object):
             emptyReport = False
             if users:
                 for user in users:
-                    report = self.query_report(start_day, days, user)
-                    if not report:
+                    user_report = self.query_report(start_day, days, user)
+                    if not user_report:
                         emptyReport = True
-                    reports[user] = report
-            report = self.query_report(start_day, days)
-            if report and not emptyReport:
+                    reports[user] = user_report
+            general_report = self.query_report(start_day, days)
+            if general_report and not emptyReport:
                 print("Found all parts of the report in storage!")
-                report['enriched_user'] = reports
-                return report
-        report = self.generate_report(start_day, days, users)
-        enriched_users = report['enriched_user']
+                general_report['enriched_user'] = reports
+                return general_report
+        general_report = self.generate_report(start_day, days, users)
+        enriched_users = general_report['enriched_user']
         if enriched_users:
             for user in enriched_users:
                 self.store_report(
                     start_day, days, enriched_users[user], user=user)
-        self.store_report(start_day, days, report, user=None)
-        return report
+        ret = copy.deepcopy(general_report)
+        del(general_report['enriched_user'])
+        self.store_report(start_day, days, general_report, user=None)
+        return ret
 
     def generate_report(self, start_day, days, users=[]):
         """
@@ -81,67 +104,21 @@ class ReportGenerator(object):
         formatted as yyyy-mm-dd, and for the period of DAYS duration
         """
         # print("Generating report for {}/{}/{}".format(start_day, days, user))
-        self.report_creator.set_users(users)
+        report_creator = report.Report()
+        report_creator.set_users(users)
         dates = self.generate_dates(start_day, days)
-        self.report_creator.set_start_date(dates[0])
-        self.report_creator.set_end_date(dates[-1])
-        # print("For the report of {} days starting {} we have dates {}".format(days, start_day, dates))
+        report_creator.set_start_date(dates[0])
+        report_creator.set_end_date(dates[-1])
+        print("For the report of {} days starting {} we have dates {}".format(days, start_day, dates))
         tables = [self.mtf.get_message_table(date) for date in dates]
-        # print("Message table names: {}".format(tables))
+        print("Message table names: {}".format(tables))
         for table in tables:
             messages = table.scan()["Items"]
             for message in messages:
-                self.report_creator.message(message)
-        self.report_creator.finalize()
-        report = self.report_creator.data()
-        return report
-
-    def summarize_report(self, report):
-        # self.report_creator.dump()
-        # self.reaction_summary(40)
-        # return
-        # print("gm activity by hour:")
-        # for x in range(0,24):
-        #    print("{}: {}".format(x, self.report_creator._data['hour'].get(x)))
-        # report = self.report_creator.data()
-        print("Report is {} chars".format(len(json.dumps(report))))
-        print("Elements of report:")
-        for item in report.keys():
-            print("\t{}".format(item))
-            # print(utils.dumps(report[item]))
-            # print("")
-        print("user activity by hour:")
-        for x in range(0, 24):
-            print("{}: {}".format(x, report['user_weekday_hour'].get(str(x))))
-
-        print("user activity by timezone:")
-        tzs = list(report['timezone'].keys())
-        tzs.sort(key=lambda x: report['timezone'][x])
-        tzs.reverse()
-        for tz in tzs:
-            print("{}: {}".format(tz, report['timezone'][tz]))
-
-        print("Report statistics:")
-        stats = report['statistics']
-        for k in stats:
-            print("\t{} : {}".format(k, stats[k]))
-
-        print("Statistics for Roy:")
-        stats = report['user_stats']['U06NSQT34']
-        print(json.dumps(stats, indent=4))
-
-    def reaction_summary(self, top=10):
-        rdict = report['reaction']
-        reactions = list(rdict.keys())
-        reactions.sort(key=lambda x: rdict[x])
-        reactions.reverse()
-        reactions = reactions[0:top]
-        for reaction in reactions:
-            print(
-                ":{}: `:{}:` :{}".format(
-                    reaction,
-                    reaction,
-                    rdict[reaction]))
+                report_creator.message(message)
+        report_creator.finalize()
+        current_report = report_creator.data()
+        return current_report
 
     def generate_dates(self, start_day, days):
         dates = []
