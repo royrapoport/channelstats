54e5a30ebb2a4a3f23b64928bafe92732340bdbc
Roy Rapoport <github@ols.inorganic.org>
2019-09-05 21:14:32 -0700

    significant enhancements

--- user_report.py
+++ user_report.py
@@ -1,20 +1,54 @@
 #! /usr/bin/env python
 
 import argparse
+import sys
 
+import user
 import slack_formatter
 import report_generator
 
+slack_formatter_obj = slack_formatter.SlackFormatter()
+rg = report_generator.ReportGenerator()
+u = user.User()
+
+def uid_for(token):
+    """
+    token is either a username or a UID.  If UID, make sure it's valid
+    If username, try to find a match and return the match's uid
+    """
+    uid = token.upper()
+    if u.get(uid):
+        return uid
+
+    matches = u.find(token)
+    if len(matches) == 0:
+        raise RuntimeError("Could not find a user with name '{}'".format(token))
+    if len(matches) > 1:
+        raise RuntimeError("Found too many matches for user {}: {}".format(token, json.dumps(matches, indent=4)))
+    uid = matches[0]['id']
+    return uid
+
+
+
 parser = argparse.ArgumentParser(description='Run a user-level Slack activity report.')
 parser.add_argument("--uid", help="Run the report for this UID")
-parser.add_argument("--fake", action="store_true", help="Use fake usernames")
 parser.add_argument("--regen", action="store_true", help="Regenerate stats even if we have them")
+parser.add_argument("--name", help="Run the report for this user")
+parser.add_argument("--nosend", action="store_true", help="Do not send report")
 args = parser.parse_args()
 
-slack_formatter_obj = slack_formatter.SlackFormatter(fake=args.fake)
-print("args.fake is {}".format(args.fake))
-rg = report_generator.ReportGenerator(fake=args.fake)
+if (not args.uid) and (not args.name):
+    raise RuntimeError("--uid|--name is required")
+
+if args.uid:
+    uid = uid_for(args.uid)
+else:
+    uid = uid_for(args.name)
+
+print("Will run report for UID {}".format(uid))
+
 latest_week_start = rg.latest_week_start()
 days = 7
-(report, previous_report) = rg.report(latest_week_start, days, users=[args.uid], force_generate=args.regen)
-slack_formatter_obj.send_report(args.uid, report, previous_report)
+send = not args.nosend
+(report, previous_report) = rg.report(latest_week_start, days, users=[uid], force_generate=args.regen)
+slack_formatter_obj.send_report(uid, report, previous_report, send=send)
