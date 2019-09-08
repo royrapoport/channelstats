7832d748be6fd18e3bf32453ccd45e9742d092ef
Roy Rapoport <github@ols.inorganic.org>
2019-09-07 20:55:06 -0700

    update

--- channel_members_log.py
+++ channel_members_log.py
@@ -1,7 +1,8 @@
+import collections
+import datetime
 import time
 import ddb
 import utils
-
 
 class ChannelMembersLog(object):
     table_name = "ChannelMembersLog"
@@ -15,6 +16,23 @@ class ChannelMembersLog(object):
 
     def token(self, ts):
         return "ts_{}".format(ts)
+
+    def is_token(self, column):
+        return column.find("ts_") == 0
+
+    def detoken(self, column):
+        assert self.is_token(column)
+        column = column.replace("ts_", "")
+        return int(column)
+
+    def update(self, cid, ts, count):
+        nows = self.token(int(ts))
+        self.table.update_item(
+            Key = {'channel_id': cid},
+            UpdateExpression='set {} = :m'.format(nows),
+            ExpressionAttributeValues={':m': count},
+            ReturnValues='UPDATED_NEW'
+        )
 
     def batch_upload(self, channels):
         now = str(int(time.time()))
@@ -36,13 +54,69 @@ class ChannelMembersLog(object):
             for row in new_rows:
                 batch.put_item(row)
         for row in update_rows:
-            self.table.update_item(
-                Key = {'channel_id': row['channel_id']},
-                UpdateExpression='set {} = :m'.format(nows),
-                ExpressionAttributeValues={':m': now},
-                ReturnValues='UPDATED_NEW'
-            )
+            cid = row['channel_id']
+            ts = now
+            count = row[nows]
+            self.update(cid, ts, count)
 
     def get(self, key):
         response = self.table.get_item(Key={'channel_id': key})
         return response.get('Item')
+
+    def make_ts(self, date):
+        """
+        Given yyyy-mm-dd return timestamp
+        """
+        y, m, d = [int(x) for x in date.split("-")]
+        dt = datetime.datetime(y, m, d, 0, 0, 0, 0)
+        return dt.timestamp()
+
+    def dump(self, cid):
+        entry = self.get(cid)
+        if not entry:
+            return 0
+        mcounts = self.get_mcounts(entry)
+        ret = collections.OrderedDict()
+        mckeys = list(mcounts.keys())
+        mckeys.sort()
+        for i in mckeys:
+            t = time.asctime(time.localtime(int(i)))
+            ret[t] = mcounts[i]
+        return ret
+
+    def get_mcounts(self, entry):
+        mcounts = {}
+        for c in entry:
+            if self.is_token(c):
+                mcounts[self.detoken(c)] = int(entry[c])
+        return mcounts
+
+    def get_count(self, cid, date, comparator, reducer):
+        """
+        Given a yyyy-mm-dd date, return the latest count before 0000 on this date
+        """
+        ts = self.make_ts(date)
+        entry = self.get(cid)
+        if not entry:
+            return 0
+        mcounts = self.get_mcounts(entry)
+        timestamps = list(mcounts.keys())
+        filtered_timestamps = [x for x in timestamps if comparator(x, ts)]
+        ts = reducer(filtered_timestamps)
+        return mcounts[ts]
+
+    def latest_count(self, cid, date):
+        """
+        Given a yyyy-mm-dd date, return the latest count before 0000 on this date
+        """
+        def f(a, b):
+            return a < b
+        return self.get_count(cid, date, f, max)
+
+    def earliest_count(self, cid, date):
+        """
+        Given a yyyy-mm-dd date, return the earliest count after 0000 on this date
+        """
+        def f(a, b):
+            return a >= b
+        return self.get_count(cid, date, f, min)
---
+++ report_utils.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env python
+
+import user
+import utils
+import channel
+
+u = user.User()
+c = channel.Channel()
+
+def override(o):
+    """
+    Assume that o is #channelname or @username
+    Return CHANNELID or USERID
+    """
+
+    if o[0] == '@':
+        token_type = "user"
+    elif o[0] == '#':
+        token_type = 'channel'
+    else:
+        raise RuntimeError("Only tokens starting with # or @ make sense to me")
+
+    if token_type == "user":
+        match = uid_for(o)
+        if match:
+            return match
+        else:
+            raise RuntimeError("Couldn't find a UID for @{}".format(o))
+    else:
+        o = o[1:]
+        match = c.get(o)
+        if match:
+            return match['channel_id']
+        else:
+            raise RuntimeError("Couldn't find a CID for #{}".format(o))
+
+def uid_for(token):
+    """
+    token is either a username or a UID.  If UID, make sure it's valid
+    If username, try to find a match and return the match's uid
+    """
+    uid = token.upper()
+    if u.get(uid):
+        return uid
+
+    if token[0] != '@':
+        raise RuntimeError("Usernames must start with '@'")
+
+    matches = u.find(token)
+    if len(matches) == 0:
+        raise RuntimeError("Could not find a user with name '{}'".format(token))
+    if len(matches) > 1:
+        raise RuntimeError("Found too many matches for user {}: {}".format(token, json.dumps(matches, indent=4)))
+    uid = matches[0]['id']
+    return uid
+
+def cid_for(token):
+    """
+    token is either a username or a UID.  If UID, make sure it's valid
+    If username, try to find a match and return the match's cid
+    """
+    cid = token.upper()
+    entry = c.get(cid)
+    if entry:
+        return entry['channel_id']
+
+    if token[0] != '#':
+        raise RuntimeError("Channel names must start with '#'")
+
+    token = token[1:]
+    entry = c.get(token)
+    if entry:
+        return entry['channel_id']
+    raise RuntimeError("Could not find a channel with name '{}'".format(token))
