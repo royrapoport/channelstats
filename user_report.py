cec8685d499dc6d2d86fd9bea7b68e3d93106520
Roy Rapoport <github@ols.inorganic.org>
2019-09-07 20:54:55 -0700

    refactor

--- user_report.py
+++ user_report.py
@@ -9,52 +9,7 @@ import channel
 import slack_user_report
 import report_generator
 
-def override(o):
-    """
-    Assume (but validate) that o is #channelname or @username
-    or CHANNELID or USERID.
-    Return CHANNELID or USERID
-    """
-
-    ou = o.upper()
-    n_o = o
-    if o[0] == "#":
-        n_o = o[1:]
-    for x in [n_o, o]:
-        match = c.get(x)
-        if match:
-            return match['channel_id']
-
-    nou = ou
-    if ou[0] == '@':
-        nou = ou[1:]
-    uitem = u.get(nou)
-    if uitem:
-        return nou
-    match = uid_for(o)
-    if match:
-        return match
-    raise RuntimeError("Can't find an override destination for {}".format(o))
-
-def uid_for(token):
-    """
-    token is either a username or a UID.  If UID, make sure it's valid
-    If username, try to find a match and return the match's uid
-    """
-    uid = token.upper()
-    if u.get(uid):
-        return uid
-
-    if token[0] != '@':
-        raise RuntimeError("Usernames must start with '@'")
-
-    matches = u.find(token)
-    if len(matches) == 0:
-        raise RuntimeError("Could not find a user with name '{}'".format(token))
-    if len(matches) > 1:
-        raise RuntimeError("Found too many matches for user {}: {}".format(token, json.dumps(matches, indent=4)))
-    uid = matches[0]['id']
-    return uid
+import report_utils
 
 parser = argparse.ArgumentParser(description='Run a user-level Slack activity report.')
 parser.add_argument("--uid", help="Run the report for this UID")
@@ -74,12 +29,12 @@ if (not args.uid) and (not args.name):
     raise RuntimeError("--uid|--name is required")
 
 if args.uid:
-    uid = uid_for(args.uid)
+    uid = report_utils.uid_for(args.uid)
 else:
-    uid = uid_for(args.name)
+    uid = report_utils.uid_for(args.name)
 
 if args.override:
-    destination = override(args.override)
+    destination = report_utils.override(args.override)
 else:
     destination = uid
 print("Will send report to {}".format(destination))
