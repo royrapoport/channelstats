bf998f3163412bf0737648604782f74172a0017d
Roy Rapoport <github@ols.inorganic.org>
2019-09-23 04:30:09 -0700

    re-create fake users and fake channels every week

--- weekly.bash
+++ weekly.bash
@@ -7,6 +7,8 @@ git pull
 set -x
 ./user_downloader.py
 ./channel_downloader.py
+./make_fake_users.py
+./make_fake_channels.py
 ./message_downloader.py
 ./user_and_channel_report
 ./slack_report.py --destination \#zmeta-statistics
