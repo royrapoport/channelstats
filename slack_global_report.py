c85578bc3f1aed6091d35364ee7d457fbbabc021
Roy Rapoport <github@ols.inorganic.org>
2020-05-03 12:19:19 -0400

    change cumulative to cumulative of total

--- slack_global_report.py
+++ slack_global_report.py
@@ -102,7 +102,7 @@ class SlackGlobalReport(object):
             it = "{}. *{}* ".format(idx + 1, self.sf.show_uid(uid))
             it += "*{:,}* words, *{:,}* messages, *{:.1f}* w/m, ".format(w, m, w_per_m)
             it += "*{:.1f}* rphw, ".format(rphw)
-            it += "*{:.1f}%*, *{:.1f}%* cumulative\n".format(per, cper)
+            it += "*{:.1f}%*, *{:.1f}%* cumulative of total\n".format(per, cper)
             blocks.append(self.sf.text_block(it))
         blocks.append(self.sf.divider())
         return blocks
