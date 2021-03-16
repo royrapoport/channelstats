07c18f38ceb666035ef8c7d9ad90df8a3e9e7f5f
Roy Rapoport <github@ols.inorganic.org>
2021-03-16 05:44:26 -0700

    store channel member information

--- channel_downloader.py
+++ channel_downloader.py
@@ -1,11 +1,13 @@
 #! /usr/bin/env python3
 
+import sys
 import time
 
 import slack_token
 import slacker
 import channel
 import channel_members_log
+import bulk_store
 
 
 class ChannelDownloader(object):
@@ -14,14 +16,45 @@ class ChannelDownloader(object):
         self.channel = channel.Channel()
         self.channel_members_log = channel_members_log.ChannelMembersLog()
         self.slack = slacker.Slacker(sname, stoken)
+        self.bulk_store = bulk_store.BulkStore()
 
     def download(self, include_private=False):
+        start_time = time.time()
         channel_types = ['public_channel']
         if include_private:
             channel_types.append('private_channel')
         channels = self.slack.get_all_channels(types=channel_types)
+        # channels = [x for x in channels if x['name'].find("ztest") == 0]
+        print("Getting channel memberships now ... ")
+        for channel in channels:
+            if channel['is_archived']:
+                continue
+            m = "Downloading members for {} ... ".format(channel['name'])
+            sys.stdout.write(m)
+            cid = channel['id']
+            try:
+                members = self.slack.get_users_for_channel(cid)
+            except:
+                sys.stdout.write("F\n")
+                sys.stdout.flush()
+                continue
+            # When a channel is archived its membership is purged
+            # We don't want to store empty memberships because we want to
+            # basically always have the last membership before the channel
+            # was archived
+            count = len(members)
+            sys.stdout.write(str(count) + "\n")
+            sys.stdout.flush()
+            if count == 0:
+                continue
+            members_string = ",".join(members)
+            self.bulk_store.set(cid, members_string)
         self.channel.batch_upload(channels)
         self.channel_members_log.batch_upload(channels)
+        end_time = time.time()
+        diff_time = int(end_time - start_time)
+        print("Downloaded channel information in {} seconds".format(diff_time)) 
+
 
 
 if __name__ == "__main__":
